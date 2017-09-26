9f2c3fd9748c1852d0ea8c6db6af05f796ef406e
ibm_admin <ibm_admin@192.168.1.7>
2017-09-26 20:33:30 +0530

    first commit

---
+++ GitProject1/src/Gitdemo/Firstcommit.java
@@ -0,0 +1,11 @@
+package Gitdemo;
+
+public class Firstcommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is my first git overview");
+	}
+
+}
